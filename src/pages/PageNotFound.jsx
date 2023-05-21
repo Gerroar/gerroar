2c739bef5310d5ab708ca9b7dc87059326802da0
Gerroar <germanariasrodriguez@gmail.com>
2023-05-21 22:20:19 +0200

    Paths interface, Home and PageNotFoun created.

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+const PageNotFound = () => {
+    return <div>This path does not exists</div>
+};
+
+export default PageNotFound;
---
+++ src/routes/paths.ts
@@ -0,0 +1,11 @@
+
+/**Defined path names and export them in an object */
+
+interface Paths {
+    [key: string] : string;
+}
+
+export default {
+    HOME: '/',
+    NOT_FOUND: '*',
+} as Paths;
